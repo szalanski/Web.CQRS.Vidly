e7bdb8398bd17b0443bb27748a7ca40c9689f891
Marcin Szlanski <szalanski91@gmail.com>
2021-03-09 19:47:13 +0100

    Add presistence layer contracts

---
+++ Web.CQRS.Vidly/Vidly.Application/Contracts/Presistence/IAsyncRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Vidly.Application.Contracts.Presistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddsAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Web.CQRS.Vidly/Vidly.Application/Contracts/Presistence/IRentalRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vidly.Domain.Entities;
+
+namespace Vidly.Application.Contracts.Presistence
+{
+    public interface IRentalRepository : IAsyncRepository<Rental>
+    {
+    }
+}
