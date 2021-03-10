4b13c6729a387cbb493068f555b6fa4f5e65ad64
Marcin Szlanski <szalanski91@gmail.com>
2021-03-10 18:31:11 +0100

    Add Vidly installator

---
+++ Web.CQRS.Vidly/Vidly.Application/AddVidlyApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vidly.Application
+{
+    public static class AddVidlyApplication
+    {
+        public static IServiceCollection Install(this IServiceCollection serivices)
+        {
+            serivices.AddAutoMapper(Assembly.GetExecutingAssembly());
+            serivices.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return serivices;
+        }
+    }
+}
