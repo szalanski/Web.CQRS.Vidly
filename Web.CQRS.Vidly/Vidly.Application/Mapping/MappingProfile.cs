27a264824299e6d360b29849278eca9d41ebf29b
Marcin Szlanski <szalanski91@gmail.com>
2021-03-10 18:30:54 +0100

    Add Mapping profile

---
+++ Web.CQRS.Vidly/Vidly.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Vidly.Application.Functions.Movies;
+using Vidly.Application.Functions.Movies.Dtos;
+using Vidly.Domain.Entities;
+
+namespace Vidly.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, MovieDto>().ReverseMap();
+        }
+    }
+}
